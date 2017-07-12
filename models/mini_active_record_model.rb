e9d5df9e1be404508c1ef162b9bf99f5402eae31
Aarosen <arirm95@gmail.com>
2017-07-12 13:32:32 -0500

    Documentacion con codigo refactorizado

--- models/mini_active_record_model.rb
+++ models/mini_active_record_model.rb
@@ -4,9 +4,7 @@ module MiniActiveRecord
 
   class Model
     attr_reader :attributes, :old_attributes
-    # self.attribute_names = [:id, :first_name, :last_name, :email, :phone,
-    #                       :birthday, :created_at, :updated_at]
-    # self.attribute_names = [:id, :name, :chef_id, :created_at, :updated_at]
+
     def self.all
       MiniActiveRecord::Model.execute("SELECT * FROM #{self}s").map do |row|
         self.new(row)
@@ -30,6 +28,62 @@ module MiniActiveRecord
       self.where('id = ?', pk).first
     end
 
+    def initialize(attributes = {})
+      attributes.symbolize_keys!
+      raise_error_if_invalid_attribute!(attributes.keys)
+
+      @attributes = {}
+
+      Meal.attribute_names.each do |name|
+        @attributes[name] = attributes[name]
+      end
+
+      Chef.attribute_names.each do |name|
+        @attributes[name] = attributes[name]
+      end
+
+      @old_attributes = @attributes.dup
+    end
+
+    def [](attribute)
+      raise_error_if_invalid_attribute!(attribute)
+
+      @attributes[attribute]
+    end
+
+    def []=(attribute, value)
+      raise_error_if_invalid_attribute!(attribute)
+
+      @attributes[attribute] = value
+    end
+
+    def chef
+      Chef.where('id = ?', self[:chef_id])
+    end
+
+    def chef=(chef)
+      self[:chef_id] = chef[:id]
+      self.save
+
+      chef
+    end
+
+    def new_record?
+      self[:id].nil?
+    end
+
+    def save
+      if new_record?
+        results = insert!
+      else
+        results = update!
+      end
+
+      # When we save, remove changes between new and old attributes
+      @old_attributes = @attributes.dup
+
+      results
+    end
 
     def self.database=(filename)
       @filename = filename
@@ -106,6 +160,36 @@ module MiniActiveRecord
       else
         value
       end
+    end
+
+    def insert!
+      self[:created_at] = DateTime.now
+      self[:updated_at] = DateTime.now
+
+      fields = self.attributes.keys
+      values = self.attributes.values
+      marks  = Array.new(fields.length) { '?' }.join(',')
+
+      insert_sql = "INSERT INTO meals (#{fields.join(',')}) VALUES (#{marks})"
+
+      results = MiniActiveRecord::Model.execute(insert_sql, *values)
+
+      # This fetches the new primary key and updates this instance
+      self[:id] = MiniActiveRecord::Model.last_insert_row_id
+      results
+    end
+
+    def update!
+      self[:updated_at] = DateTime.now
+
+      fields = self.attributes.keys
+      values = self.attributes.values
+
+      update_clause = fields.map { |field| "#{field} = ?" }.join(',')
+      update_sql = "UPDATE meals SET #{update_clause} WHERE id = ?"
+
+      # We have to use the (potentially) old ID attribute in case the user has re-set it.
+      MiniActiveRecord::Model.execute(update_sql, *values, self.old_attributes[:id])
     end
 
   end
--- test.rb
+++ test.rb
@@ -8,14 +8,14 @@ end
 assert chef1 = Chef.all
 # chef2 = Chef.create(first_name: "Aaron", last_name: "Rosenzweig", email:"aaron@gmail.com", phone:"930-028-2908 x7555", birthday:Time.now)
 chef3 = Chef.where("first_name = 'Aaron'")
-puts chef4 = Chef.find(1)
+chef4 = Chef.find(1)
 
 
 # assert meal[:name] == 'Chicken Burger'
 meal1 = Meal.all
 # meal2 = Meal.create(name: "Pasta Carbonara", chef_id: 1)
 meal3 = Meal.where("name = ?", 'Pasta Carbonara')
-puts meal4 = Meal.find(1)
+meal4 = Meal.find(1)
 
 
 puts "finished"
